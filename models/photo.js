13c88337f0851f8cc6a3337d61a646111b8e76f0
James Kirawan <jameskirawan@gmail.com>
2022-05-08 20:12:05 +0700

    add photo migration and validation photo

---
+++ models/photo.js
@@ -0,0 +1,45 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class Photo extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      this.belongsTo(models.User, {
+        foreignKey: "UserId",
+        as: "user",
+      });
+    }
+  }
+  Photo.init(
+    {
+      title: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      caption: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+      },
+      poster_image_url: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+        validate: {
+          isUrl: {
+            msg: "Must Be A Valid Url",
+          },
+        },
+      },
+      UserId: DataTypes.INTEGER,
+    },
+    {
+      sequelize,
+      modelName: "Photo",
+    }
+  );
+  return Photo;
+};
