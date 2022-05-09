eed319c9a6fa9f259c4bd4913d3d176f761ae724
James Kirawan <jameskirawan@gmail.com>
2022-05-09 18:56:05 +0700

    testing webhook

--- controllers/photo.controller.js
+++ controllers/photo.controller.js
@@ -14,7 +14,6 @@ exports.getPhoto = async (req, res) => {
     ],
   })
     .then((photo) => {
-      // console.log("test");
       res.status(200).json({
         photos: {
           id: photo[0].dataValues.id,
