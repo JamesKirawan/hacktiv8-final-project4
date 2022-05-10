4c4c3a1d492615d7b9e227f51becd0a78a398df3
James Kirawan <jameskirawan@gmail.com>
2022-05-10 20:14:29 +0700

    fix deployment script

--- config/config.js
+++ config/config.js
@@ -4,7 +4,7 @@ module.exports = {
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
-    port: process.env.DB_PORT,
+    port: process.env.PORT,
     host: process.env.DB_HOST,
     dialect: "postgres",
     dialectOptions: {
