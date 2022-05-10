fe28b9d0b310329e139113e9c4e68fa3458bc6d6
James Kirawan <jameskirawan@gmail.com>
2022-05-10 19:36:26 +0700

    fix deployment script

--- config/config.js
+++ config/config.js
@@ -9,14 +9,16 @@ module.exports = {
     dialect: "postgres",
   },
   production: {
-    user: process.env.DB_USER,
+    username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
     host: process.env.DB_HOST,
     dialect: "postgres",
-    ssl: {
-      require: true,
-      rejectUnauthorized: false,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
     },
   },
   test: {
